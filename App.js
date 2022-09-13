1950da4fae84de2a9701be78542fb91cd1d2c1ab
Phung Minh Hieu <hieuphung1206@Phungs-MacBook-Pro.local>
2022-09-13 17:34:27 +0700

    02 su dung useState quan ly state

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, StatusBar, StyleSheet, Text, View, Linking} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('The default name');
+
+  const [session, setSession] = useState({number: 6, title: 'state'});
+
+  const onClickHandler = () => {
+    setName('This is the new name');
+    setSession({number: 7, title: 'style'});
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <View style={styles.body}>
-        <Text style={styles.text}>Xin chao cac ban</Text>
-        <Button
-          title="Click me"
-          onPress={() => {
-            Linking.openURL('https://google.com');
-          }}
-        />
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>
+          This is session number {session.number} and about {session.title}
+        </Text>
+        <Button title="Update name" onPress={onClickHandler} />
       </View>
     </>
   );
