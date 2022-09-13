00018a282fe31714a591f1ddd662e685c22beac8
Phung Minh Hieu <hieuphung1206@Phungs-MacBook-Pro.local>
2022-09-13 23:26:20 +0700

    05 show list item use jsx but not scroll in virtual machine

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {Button, StatusBar, StyleSheet, Text, View, Linking} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View, Linking} from 'react-native';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+    {key: 9, item: 'Item 9'},
+    {key: 10, item: 'Item 10'},
+    {key: 11, item: 'Item 11'},
+    {key: 12, item: 'Item 12'},
+  ]);
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.body}>
-        <Text style={styles.text}>Xin chao cac ban</Text>
-        <Button
-          title="Click me"
-          onPress={() => {
-            Linking.openURL('https://google.com');
-          }}
-        />
-      </View>
-    </>
+    <View style={styles.body}>
+      {items.map(i => {
+        return (
+          <View style={styles.item} key={i.key}>
+            <Text style={styles.text}>{i.item}</Text>
+          </View>
+        );
+      })}
+    </View>
   );
 };
 
@@ -30,8 +34,14 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: '#ffffff',
-    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: 35,
+  },
+  item: {
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 1,
   },
   text: {
     fontSize: 20,
